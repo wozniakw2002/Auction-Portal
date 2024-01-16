2bc21d8bd9ff36f3b5487c28a82875a0737e536b
Wiktor <103779975+wozniakw2002@users.noreply.github.com>
2024-01-16 22:25:36 +0100

    URL tests added

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import home, createAuction, auction, updateAuction
+class TestUrls(SimpleTestCase):
 
-# Create your tests here.
+    def test_home_url_is_resolved(self):
+        url = reverse('home')
+        self.assertEqual(resolve(url).func, home)
+
+    def test_create_auction_url_is_resolved(self):
+        url = reverse('create-auction')
+        self.assertEqual(resolve(url).func, createAuction)
+
+    def test_auction_url_is_resolved(self):
+        url = reverse('auction', args=['pk'])
+        self.assertEqual(resolve(url).func, auction)
+
+    def test_update_auction_url_is_resolved(self):
+        url = reverse('update-auction', args=['pk'])
+        self.assertEqual(resolve(url).func, updateAuction)
+
+
+
